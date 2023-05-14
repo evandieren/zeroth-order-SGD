2a1a202ab4fc15fcc93a91192ddbfb325128603c
Desktop.Local <edmund.hofflin@gmail.com>
2023-05-15 01:33:39 +0200

    MLOpt: project=small progress

--- simulations/zeroth_variants.py
+++ simulations/zeroth_variants.py
@@ -5,28 +5,34 @@ from torch.autograd.function import once_differentiable
 
 from difference_methods import one_point_estimate, two_point_estimate, coordinate_estimate
 
+def forward_helper(x, w, b):
+    o = x.mm(w.t())
+    if b is not None:
+        o += b.unsqueeze(0).expand_as(o)
+    return o
+
 class LinearFunction_Zeroth(Function):
     """ Version of affine function that uses zeroth order derivative estimation for gradient calculation """
     
-
     @staticmethod
     def forward(ctx, input, weight, bias=None, difference_method : str = None, mu : float = None, n : int = None):
-        # Save values for backward
+        # Save differentiable values for backward
         ctx.save_for_backward(input, weight, bias)
+
+        # Save non_differentiable values for backwards
+        f = lambda x: forward_helper(x, w=weight, b=bias)
         if difference_method == "one":
-            ctx.diff = lambda x, f: one_point_estimate(x, f, mu, n)
+            ctx.diff = lambda x: one_point_estimate(x, f, mu, n)
         elif difference_method == "two":
-            ctx.diff = lambda x, f: two_point_estimate(x, f, mu, n)
+            ctx.diff = lambda x: two_point_estimate(x, f, mu, n)
         elif difference_method == "coord":
-            ctx.diff = lambda x, f: coordinate_estimate(x, f, mu)
+            ctx.diff = lambda x: coordinate_estimate(x, f, mu)
         else:
             raise ValueError(f"Input {difference_method=} must be 'one', 'two', or 'coord'.")
         ctx.mark_non_differentiable(difference_method, mu, n)
-        # Calculate output
-        output = input.mm(weight.t())
-        if bias is not None:
-            output += bias.unsqueeze(0).expand_as(output)
-        return output
+        
+        # Return output
+        return f(input)
 
     @staticmethod
     @once_differentiable
@@ -35,18 +41,10 @@ class LinearFunction_Zeroth(Function):
         input, weight, bias = ctx.saved_tensors
         # Initialise gradients to None
         grad_input = grad_weight = grad_bias = None
-
-        # Setup function for difference methods
-        def f(x=input, w=weight, b=bias):
-            o = x.mm(w.t())
-            if b is not None:
-                o += b.unsqueeze(0).expand_as(o)
-            return o
         
         # Calculate gradients if needed
         if ctx.needs_input_grad[0]:
-            my_f = lambda x: f(x=x)
-            grad_input = ctx.diff(input, my_f) # include grad_output??
+            grad_input = ctx.diff(input) # include grad_output??
         if ctx.needs_input_grad[1]:
             grad_weight = grad_output.t().mm(input)
         if bias is not None and ctx.needs_input_grad[2]:
