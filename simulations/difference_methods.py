d2ea57f4d9247ee8bb22b8ed02d96b51a2b768ce
Edmund Wolf Hofflin <edmund.hofflin@gmail.com>
2023-05-19 13:59:01 +0200

    back-end: cleaning

--- simulations/difference_methods.py
+++ simulations/difference_methods.py
@@ -10,17 +10,12 @@ def n_sphere_sample(shape):
 
 def one_point_estimate(f, x : np.ndarray, mu : float, n : int = 1):
     """ Single point estimate for derivative """
-    # print(f"x = {x}, {type(x)}") #, {x.dtype}, {x.shape}")
-    # Checks
     assert n > 0, f"Input {n=} must be greater than zero."
     # Calculate output
     output = torch.zeros_like(x)
     for _ in range(0,n):
         # Sample
         u_i = n_sphere_sample(x.shape)
-        # print(f"u_i = {u_i}, {type(u_i)}, {u_i.dtype}, {u_i.shape}")
-        # print(f"x+mu*u_i = {x + mu * u_i}, {type(x + mu * u_i)}, {(x + mu * u_i).dtype}, {(x + mu * u_i).shape}")
-        # print(f"f(x+mu*u_i) = {f(x + mu * u_i)}, {type(f(x + mu * u_i))}, {f(x + mu * u_i).dtype}, {f(x + mu * u_i).shape}")
         output += f(x + mu * u_i).sum() * u_i
     # Return
     return (np.prod(x.size())/(n*mu)) * output
@@ -41,7 +36,6 @@ def two_point_estimate(f, x : np.ndarray, mu : float, n : int = 1):
 
 def coordinate_estimate(f, x : np.ndarray, mu : float):
     """ Coordinate estimate for derivative """
-    # print(f"x = {x}, {type(x)}") #, {x.dtype}, {x.shape}")
     # Calculate output
     output = torch.zeros_like(x)
     for i in range(0,np.prod(output.size())):
@@ -50,9 +44,6 @@ def coordinate_estimate(f, x : np.ndarray, mu : float):
         e_i[i] = 1
         e_i = e_i.reshape(x.shape)
         # Calculate next term
-        # print(f"e_i = {e_i}, {type(e_i)}, {e_i.dtype}, {e_i.shape}")
-        # print(f"x + mu * e_i = {x + mu * e_i}, {(x + mu * e_i).shape}")
-        # print(f"f(x + mu * e_i) = {f(x + mu * e_i)}, {(f(x + mu * e_i)).shape}")
         output += (f(x + mu * e_i) - f(x)).sum() * e_i
     # Return
     return (1/mu) * output
